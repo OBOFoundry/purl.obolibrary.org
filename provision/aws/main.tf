4328ba2d1fd6a685be5bd81633c1bdea917b3dc8
abessiari <aessiari@lbl.gov>
2022-01-19 20:41:38 -0800

    creating elastic ip. using init=true in docker compose. added start and down services ansible scripts

--- provision/aws/main.tf
+++ provision/aws/main.tf
@@ -1,33 +1,5 @@
-data "aws_ami" "ubuntu" {
-    most_recent = true
-
-    filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-    }
-
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
-
-    owners = ["099720109477"]
-}
-
-// Search for eip by allocation_id
-// If found data.aws_eip.purl_eip.public_ip will be the public ip.
-data "aws_eip" "purl_eip" {
-  id = var.eip_alloc_id
-}
-
-resource "aws_eip_association" "eip_assoc" {
-  instance_id   = aws_instance.purl_server.id
-  allocation_id = data.aws_eip.purl_eip.id
-}
-
 resource "aws_instance" "purl_server" {
-  // This ami works for use-east-1 "ami-0dd76f917833aac4b"   
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = var.ami
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.purl_sg.id]
   subnet_id              = aws_subnet.purl_app_stack_public_subnet.id
@@ -38,23 +10,23 @@ resource "aws_instance" "purl_server" {
     device_name           = "/dev/sda1"
     delete_on_termination = true
     tags                  = var.tags
-    volume_size           = 100
+    volume_size           = var.disk_size 
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "curl -fsSL https://get.docker.com -o /tmp/get-docker.sh",
-      "sudo sh /tmp/get-docker.sh",
-      "sudo usermod -aG docker ubuntu",
-      "sudo apt-get install -y docker-compose",
+  lifecycle {
+    ignore_changes = [
+      ebs_block_device,
+      tags,
     ]
-
-    connection {
-      host        = aws_instance.purl_server.public_ip
-      type        = "ssh"
-      user        = "ubuntu"
-      agent       = false
-      private_key = file(var.private_key_path)
-    }
   }
+}
+
+resource "aws_eip" "purl_eip" {
+  vpc   = true
+  tags  = var.tags
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.purl_server.id
+  allocation_id = aws_eip.purl_eip.id
 }
